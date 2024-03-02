7b96798290ad1b56dcd2d61c2918bb34dde4fd92
DurandAntonin <antonin.durand576@gmail.com>
2024-03-02 12:56:58 +0100

    Fonctionnalité Line

--- src/Line.java
+++ src/Line.java
@@ -14,13 +14,27 @@ public class Line extends Shape {
      */
     private Point p2;
 
+
+    /**
+     * @param px 
+     * @param py
+     */
+    public Line(int parId, COLOR parColor, int parThickness, Point parP1, Point parP2) {
+        //on initialise les champs
+        super(parId, 0, 0, parColor, parThickness);
+        this.p1 = parP1;
+        this.p2 = parP2;
+    }
+
     /**
      * @param parP1 
      * @param parP2
      */
     public Line(Point parP1, Point parP2) {
-        // TODO implement here
+        //on initialise les champs
         super(null, null, null, null, null);
+        this.p1 = parP1;
+        this.p2 = parP2;
     }
 
     /**
@@ -28,14 +42,61 @@ public class Line extends Shape {
      * 
      * @return
      */
-    public List<Pixel> draw() {
-        // TODO implement here
-        return null;
+    public ArrayList<Pixel> draw() {
+        ArrayList<Pixel> pixelLine = new ArrayList<>();
+
+        int dx = Math.abs(this.p2.getPosX() - this.p1.getPosX());
+        int dy = Math.abs(this.p2.getPosY() - this.p1.getPosY());
+
+        int cumul;
+        int xinc = (dx > 0) ? 1 : -1;
+        int yinc = (dy > 0) ? 1 : -1;
+        int x = this.p1.getPosX();
+        int y = this.p1.getPosY();
+
+        pixelLine.add(new Pixel(x, y, COLOR.BLACK));
+        if (dx > dy){
+            cumul = dx /2;
+            for (int i=1; i<=dx; i++){
+                x += xinc;
+                cumul += dy;
+                if (cumul >= dx){
+                    cumul -= dx;
+                    y += yinc;
+                }
+
+                pixelLine.add(new Pixel(x, y, COLOR.BLACK));
+            }
+        }
+        else{
+            cumul = dy / 2;
+            for (int i=1; i<=dy; i++){
+                y += yinc;
+                cumul += dx;
+                if (cumul >= dy){
+                    cumul -= dy;
+                    x += xinc;
+                }
+
+                pixelLine.add(new Pixel(x, y, COLOR.BLACK));
+            }
+        }
+
+        return pixelLine;
     }
 
     @Override
     public boolean equals(Object object){
-        return false;
+        //on regarde si les 2 variables font référence au meme objet
+        if (this == object)
+            return true;
+
+        //on regarde si l'objet à comparer est null ou qu'il n'est pas de la meme classe que this
+        if (object == null || !this.getClass().equals(object.getClass()))
+            return false;
+        
+        //on vérifie que les valeurs des champs sont identiques
+        return this.getId() == ((Line) object).getId() && this.getP1().equals(((Line) object).getP1()) && this.getP2().equals(((Line) object).getP2()) && this.getX() == ((Line) object).getX() && this.getY() == ((Line) object).getY() && this.getColor() == ((Line) object).getColor() && this.getThickness() == ((Line) object).getThickness();
     }
 
     /**
@@ -43,7 +104,7 @@ public class Line extends Shape {
      */
     public String toString() {
         // TODO implement here
-        return "";
+        return "Line [" + super.toString() + ", p1: " + this.p1.toString() + ", p2: " + this.p2.toString() + "]";
     }
 
     public Point getP1() {
--- src/LineTest.java
+++ src/LineTest.java
@@ -1,6 +1,6 @@
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,15 +25,15 @@ public class LineTest {
         l1 = new Line(p1, p2);
         px1 = new Pixel(3,9);
         px2 = new Pixel(4,9);
-        px3 = new Pixel(5,8);
-        px4 = new Pixel(6,8);
-        px5 = new Pixel(7,7);
-        px6 = new Pixel(8,7);
-        px7 = new Pixel(9,6);
-        px8 = new Pixel(10,6);
-        px9 = new Pixel(11,5);
-        px10 = new Pixel(12,5);
-        px11 = new Pixel(13,5);
+        px3 = new Pixel(5,10);
+        px4 = new Pixel(6,10);
+        px5 = new Pixel(7,11);
+        px6 = new Pixel(8,11);
+        px7 = new Pixel(9,11);
+        px8 = new Pixel(10,12);
+        px9 = new Pixel(11,12);
+        px10 = new Pixel(12,13);
+        px11 = new Pixel(13,13);
 
     }
 
@@ -57,8 +57,9 @@ public class LineTest {
 
     @Test
     public void testDraw() {
-        List<Pixel> pixels = Arrays.asList(px1, px2, px3, px4, px5, px6, px7, px8, px9, px10, px11);
-        assertArrayEquals(pixels.toArray(), l1.draw().toArray());
+        List<Pixel> pixelsExcepted = Arrays.asList(px1, px2, px3, px4, px5, px6, px7, px8, px9, px10, px11);
+        ArrayList<Pixel> pixelsActual = l1.draw();
+        assertEquals(pixelsExcepted, pixelsActual);
     }
 
     @Test
@@ -75,7 +76,7 @@ public class LineTest {
     @Test
     public void testToString(){
         String l1StringActual = l1.toString();
-        String l1StringExcepted = "Line [p1: " + l1.getP1().toString() + ", p2: " + l1.getP2().toString() + "]";
+        String l1StringExcepted = "Line [id: " + l1.getId() + ", x: " + l1.getX() + ", y: " + l1.getY() + ", color: " + l1.getColor() + ", thickness: " + l1.getThickness() + ", p1: " + l1.getP1().toString() + ", p2: " + l1.getP2().toString() + "]";
 
         Assert.assertEquals(l1StringExcepted, l1StringActual);
     }
